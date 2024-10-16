507a8a6636aa29ffb63da9ab1722094de4d60030
Joshua <105057431+Boricuaso17@users.noreply.github.com>
2024-10-16 19:36:52 -0400

    Added puertas y ventanas folders

--- Irving/index.js
+++ Irving/index.js
@@ -1,11 +1,28 @@
-function darkMode (){
-    $("body").toggleClass("dark-css");
+function darkMode() {
+  $("body").toggleClass("dark-css");
 
-    var buttonTxt = $("button").html();
+  var buttonTxt = $("button").html();
 
-    if(buttonTxt === '<i class="fa-solid fa-moon"></i>') {
-        $("button").html('<i class="fa-solid fa-sun"></i>');
-    }
-    else 
-    $("button").html('<i class="fa-solid fa-moon"></i>');
+  if (buttonTxt === '<i class="fa-solid fa-moon"></i>') {
+    $("button").html('<i class="fa-solid fa-sun"></i>');
+  } else $("button").html('<i class="fa-solid fa-moon"></i>');
+}
+
+var x = 0;
+
+function carouselPhotos(picker) {
+  
+  switch (picker) {
+    case 'puertas':
+      x++;
+      if(x > 7)
+        x = 1;
+              var puertasSlide = document.getElementById('puertas');
+
+              puertasSlide.setAttribute('src', './images/puertas/puerta'+ x +'.jpg');
+              console.log('./images/puertas/puerta'+ x +'.jpg')
+      break;
+    case 'ventanas':
+      break;
+  }
 }
